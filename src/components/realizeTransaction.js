6d0c870fe8380af8b40e4127cd5cbb32368282e8
Jeyssson <61421639+jeyssson4k@users.noreply.github.com>
2022-08-07 02:37:05 -0500

    Update realizeTransaction.js

--- src/components/realizeTransaction.js
+++ src/components/realizeTransaction.js
@@ -1,24 +1,30 @@
 const { users } = require("../users/users.json");
 
 const realizeTransaction = (transactionCode, user, { account, amount }) => {
-  let isTransactionOK = false;
+  console.log(account, users);
+  let isOK = false;
   if (transactionCode === 1) {
     users.forEach((e) => {
       if (e.account === account) {
         e.balance += amount;
         user.balance -= amount;
-        isTransactionOK = true;
+        console.log(users);
+        alert("Transacción exitosa");
+        isOK = true;
       }
     });
+    if (isOK === false)
+      alert("Transacción fallida. El número de cuenta es inválido.");
   } else if (transactionCode === 2) {
     users.forEach((e) => {
       if (e.account === account) {
         e.balance -= amount;
-        isTransactionOK = true;
+        console.log(users);
+        return true;
       }
     });
   }
-  return isTransactionOK;
 };
 
 export default realizeTransaction;
+
